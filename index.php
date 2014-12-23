5560323faf842d0c3c15e4aed132f4de120f8b76
Mark Ragno <mark@ragno76.com>
2014-12-23 15:57:36 -0700

    first push

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Next Day Appraisal</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="page">
+
+        <div class="hero">
+
+            <div class="header">
+                <header>
+                    <nav>
+                        
+                    </nav>
+                </header>
+            </div>
+
+            div.
+        </div>
+
+
+    </div>
+
+
+    
+</body>
+</html>
