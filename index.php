b4302e08a59da32c01e727b0b0f36fff726d8004
Mark Ragno <mark@ragno76.com>
2014-12-23 18:56:00 -0700

    2nd push

--- index.php
+++ index.php
@@ -8,23 +8,100 @@
 <body>
     <div class="page">
 
-        <div class="hero">
-
-            <div class="header">
-                <header>
-                    <nav>
-                        
-                    </nav>
-                </header>
+        <header>
+            <div class="logo">
+                <img src="" alt="next day logo">
             </div>
+            <nav>
+                <a href="">How it Works</a>
+                <a href="">Support</a>
+                <a href="">Realtors</a>
+                <a href="">Homeowners</a>
+            </nav>
+            <div class="order"><a href="">Order Now</a></div>
+        </header>
 
-            div.
-        </div>
+        <section class="intro">
+            <div class="hero">
+                <h1>FIND OUT WHAT YOUR HOME IS WORTH - <br>IN LESS THAN 24 HOURS</h1>
+                <div>
+                    <a href="">ORDER NOW</a>
+                </div>
+                <div class="objective">
+                    <h2>Covering all of the Valley!</h2>
+                    <a href="">Locations</a>
+                </div>
+                <div class="objective">
+                    <h2>24-hour $79 desktop product</h2>
+                    <a href="">Pricing</a>
+                </div>
+                <div class="objective">
+                    <h2>Full service appraisal team</h2>
+                    <a href="">Products</a>
+                </div>
+            </div>
+        </section>
 
+        <section class="turntime">
+            <div class="content">
+                <h1>UNBEATABLE RESPONSE TIMES</h1>
+                <h2>GUARANTEED BACK WITHIN 24 HOURS OF ORDERING</h2>
+                <h2>WILL BE VIEWABLE ONLINE</h2>
+            </div>
+            <div class="image">
+                
+            </div>    
+        </section>
+
+        <section class="humanvalue">
+            <div class="content">
+                <h1>HUMANS DETERMINING HUMAN VALUE</h1>
+                <p>What do machines know about vistas, views, and creature comforts?  Humans need to be the ones to determine value.</p>
+            </div>
+            <div class="image">
+                
+            </div>
+        </section>
+
+        <section class="realtors">
+            <div class="content">
+                <h1>REALTORS:</h1>
+                <ul>
+                    <li>CMA's</li>
+                    <li>Certified Appraiser for your listing portfolios</li>
+                    <li>Home Measuring</li>
+                </ul>
+            </div>
+            <div class="image">
+                
+            </div>
+        </section>
+
+        <section class="upgrade">
+            <div class="content">
+                <h1>UPGRADE YOUR REPORT</h1>
+                <p>If you order our desktop or drive-by product, we will perform a full appraisal for you minus the cost of the original product price.</p>
+            </div>
+            <div class="image">
+                
+            </div>
+        </section>
+    
+        <section class="institutions">
+            <div class="content">
+                <h1>WE WILL WORK WITH YOUR LENDER</h1>
+                <p>If you like our desktop and wish to use us to refinance or purchase a home - we will make every effort to seamlessly transition the report to the lender.</p>
+            </div>
+            <div class="image">
+                
+            </div>
+        </section>
+
+        <footer>
+            <h3>NEXT DAY APPRAISAL, LLC</h3>
+        </footer>
 
     </div>
-
-
     
 </body>
 </html>
