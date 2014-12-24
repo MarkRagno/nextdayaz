9d979b98832afcba3bed27fd1b8515281f2b5cbb
Mark Ragno <mark@ragno76.com>
2014-12-23 21:37:21 -0700

    black/white/orange

--- index.php
+++ index.php
@@ -18,13 +18,17 @@
                 <a href="">Realtors</a>
                 <a href="">Homeowners</a>
             </nav>
-            <div class="order"><a href="">Order Now</a></div>
+            <div class="order">
+                <a href="">Order Now</a>
+            </div>
         </header>
 
         <section class="intro">
             <div class="hero">
                 <article>
-                    <h1>FIND OUT WHAT YOUR HOME IS WORTH IN <span>LESS THAN 24 HOURS</span></h1>
+                    <h2>Get your home's value for</h2>
+                    <h1><span id="big">$79</span></h1>
+                    <h1>IN LESS THAN 24 HOURS</h1>
                 </article>
                 <div class="ordernow">
                     <a href="">ORDER NOW</a>
@@ -51,6 +55,8 @@
                     <h1>UNBEATABLE RESPONSE TIMES</h1>
                     <h2>GUARANTEED BACK WITHIN 24 HOURS OF ORDERING</h2>
                     <h2>WILL BE VIEWABLE ONLINE</h2>
+                    <p>Check status online</p>
+                    <p>Email final report or download from website</p>
                 </div>
                 <div class="image">
                     
@@ -59,7 +65,7 @@
 
             <section class="humanvalue">
                 <div class="content">
-                    <h1>HUMANS DETERMINING HUMAN VALUE</h1>
+                    <h1>REAL PEOPLE DETERMINING VALUE</h1>
                     <p>What do machines know about vistas, views, and creature comforts?  Humans need to be the ones to determine value.</p>
                 </div>
                 <div class="image">
@@ -74,6 +80,7 @@
                         <li>CMA's</li>
                         <li>Certified Appraiser for your listing portfolios</li>
                         <li>Home Measuring</li>
+                        <li>Register for bulk discount</li>
                     </ul>
                 </div>
                 <div class="image">
@@ -83,7 +90,7 @@
 
             <section class="upgrade">
                 <div class="content">
-                    <h1>UPGRADE YOUR REPORT</h1>
+                    <h1>UPGRADE YOUR APPRAISAL</h1>
                     <p>If you order our desktop or drive-by product, we will perform a full appraisal for you minus the cost of the original product price.</p>
                 </div>
                 <div class="image">
@@ -94,16 +101,20 @@
             <section class="institutions">
                 <div class="content">
                     <h1>WE WILL WORK WITH YOUR LENDER</h1>
-                    <p>If you like our desktop and wish to use us to refinance or purchase a home - we will make every effort to seamlessly transition the report to the lender.</p>
+                    <p>If you wish to use our appraisal to refinance or purchase a home - we will make every effort to seamlessly transition the report to the lender.</p>
                 </div>
                 <div class="image">
                     
                 </div>
             </section>
         </main>
-        
+
         <footer>
             <h3>NEXT DAY APPRAISAL, LLC</h3>
+            <ul>
+                <li>p. 855-391-7500</li>
+                <li>e. appraisals@nextdayaz.com</li>
+            </ul>
         </footer>
 
     </div>
