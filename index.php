40da158d2660342ef3a67d37ae50eda0354e652b
Mark Ragno <mark@ragno76.com>
2014-12-23 19:57:26 -0700

    3rd push

--- index.php
+++ index.php
@@ -23,10 +23,14 @@
 
         <section class="intro">
             <div class="hero">
-                <h1>FIND OUT WHAT YOUR HOME IS WORTH - <br>IN LESS THAN 24 HOURS</h1>
-                <div>
+                <article>
+                    <h1>FIND OUT WHAT YOUR HOME IS WORTH IN <span>LESS THAN 24 HOURS</span></h1>
+                </article>
+                <div class="ordernow">
                     <a href="">ORDER NOW</a>
                 </div>
+            </div>
+            <div class="objectives">
                 <div class="objective">
                     <h2>Covering all of the Valley!</h2>
                     <a href="">Locations</a>
@@ -41,62 +45,63 @@
                 </div>
             </div>
         </section>
+        <main>
+            <section class="turntime">
+                <div class="content">
+                    <h1>UNBEATABLE RESPONSE TIMES</h1>
+                    <h2>GUARANTEED BACK WITHIN 24 HOURS OF ORDERING</h2>
+                    <h2>WILL BE VIEWABLE ONLINE</h2>
+                </div>
+                <div class="image">
+                    
+                </div>    
+            </section>
 
-        <section class="turntime">
-            <div class="content">
-                <h1>UNBEATABLE RESPONSE TIMES</h1>
-                <h2>GUARANTEED BACK WITHIN 24 HOURS OF ORDERING</h2>
-                <h2>WILL BE VIEWABLE ONLINE</h2>
-            </div>
-            <div class="image">
-                
-            </div>    
-        </section>
+            <section class="humanvalue">
+                <div class="content">
+                    <h1>HUMANS DETERMINING HUMAN VALUE</h1>
+                    <p>What do machines know about vistas, views, and creature comforts?  Humans need to be the ones to determine value.</p>
+                </div>
+                <div class="image">
+                    
+                </div>
+            </section>
 
-        <section class="humanvalue">
-            <div class="content">
-                <h1>HUMANS DETERMINING HUMAN VALUE</h1>
-                <p>What do machines know about vistas, views, and creature comforts?  Humans need to be the ones to determine value.</p>
-            </div>
-            <div class="image">
-                
-            </div>
-        </section>
+            <section class="realtors">
+                <div class="content">
+                    <h1>REALTORS:</h1>
+                    <ul>
+                        <li>CMA's</li>
+                        <li>Certified Appraiser for your listing portfolios</li>
+                        <li>Home Measuring</li>
+                    </ul>
+                </div>
+                <div class="image">
+                    
+                </div>
+            </section>
 
-        <section class="realtors">
-            <div class="content">
-                <h1>REALTORS:</h1>
-                <ul>
-                    <li>CMA's</li>
-                    <li>Certified Appraiser for your listing portfolios</li>
-                    <li>Home Measuring</li>
-                </ul>
-            </div>
-            <div class="image">
-                
-            </div>
-        </section>
-
-        <section class="upgrade">
-            <div class="content">
-                <h1>UPGRADE YOUR REPORT</h1>
-                <p>If you order our desktop or drive-by product, we will perform a full appraisal for you minus the cost of the original product price.</p>
-            </div>
-            <div class="image">
-                
-            </div>
-        </section>
-    
-        <section class="institutions">
-            <div class="content">
-                <h1>WE WILL WORK WITH YOUR LENDER</h1>
-                <p>If you like our desktop and wish to use us to refinance or purchase a home - we will make every effort to seamlessly transition the report to the lender.</p>
-            </div>
-            <div class="image">
-                
-            </div>
-        </section>
-
+            <section class="upgrade">
+                <div class="content">
+                    <h1>UPGRADE YOUR REPORT</h1>
+                    <p>If you order our desktop or drive-by product, we will perform a full appraisal for you minus the cost of the original product price.</p>
+                </div>
+                <div class="image">
+                    
+                </div>
+            </section>
+        
+            <section class="institutions">
+                <div class="content">
+                    <h1>WE WILL WORK WITH YOUR LENDER</h1>
+                    <p>If you like our desktop and wish to use us to refinance or purchase a home - we will make every effort to seamlessly transition the report to the lender.</p>
+                </div>
+                <div class="image">
+                    
+                </div>
+            </section>
+        </main>
+        
         <footer>
             <h3>NEXT DAY APPRAISAL, LLC</h3>
         </footer>
